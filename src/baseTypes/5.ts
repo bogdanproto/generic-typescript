fa163915f15aeb9156b283267359a1d40faeb630
Bogdan Protasenko <b.protasenko@gmail.com>
2023-11-09 12:12:34 +0100

    done

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,7 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+let union: number | string;
+
+type switcher = "enable" | "disable";
+let literal: switcher;
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,16 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface PageInfo {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T> {
+  constructor(public props: T) {}
+}
+
+class Page extends Component<PageInfo> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
